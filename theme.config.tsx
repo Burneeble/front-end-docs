83c51aa429299f435c40f2bf5a34904859e9ae1a
Alessandro privitera <63925908+DogeBao@users.noreply.github.com>
2023-07-17 19:16:47 +0200

    update

---
+++ theme.config.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+  logo: <span>Web3 Docs</span>,
+  project: {
+    link: 'https://github.com/shuding/nextra-docs-template',
+  },
+  chat: {
+    link: 'https://discord.com',
+  },
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  footer: {
+    text: 'Web3 Docs',
+  },
+}
+
+export default config
