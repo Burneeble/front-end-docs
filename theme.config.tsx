3df9e061cefc73ad3f95c51bd110e9dd3528141d
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2023-07-30 19:16:51 +0200

    icons fix

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,19 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
+import "./pages/_app";
 
 const config: DocsThemeConfig = {
   logo: <span>Web3 Docs</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/shuding/nextra-docs-template",
   },
   chat: {
-    link: 'https://discord.com',
+    link: "https://discord.com",
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/shuding/nextra-docs-template",
   footer: {
-    text: 'Web3 Docs',
+    text: "Web3 Docs",
   },
-}
+};
 
-export default config
+export default config;
